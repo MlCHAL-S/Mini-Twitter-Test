5e83e57e4c9a7e35e32a79f6c8b65351b2e82e9b
Jakub Sienski <sienski.ks@gmail.com>
2024-10-11 11:33:39 +0200

    added adding

--- server/server.py
+++ server/server.py
@@ -2,6 +2,12 @@
 This is server.py module
 """
 
+
 def receive_message(message: str) -> str:
     """This function receives a message"""
     return message
+
+
+def add_numbers(a: int, b: int) -> int:
+    """This function adds numbers"""
+    return a + b
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,14 @@
 """
 This module is for testing server
 """
-from server.server import receive_message
+from server.server import receive_message, add_numbers
 
 
 def test_receive_message():
     """This function tests receiving a message"""
     assert receive_message('Hello, World!') == 'Hello, World!'
+
+
+def test_add_numbers():
+    """This function tests adding numbers"""
+    assert add_numbers(2, 10) == 12
